4e00dd45c7df1205d0c3622b275c25bce8f6ab68
Carl Furrow <carl.furrow@betterup.co>
2022-11-21 17:16:31 -0500

    copilot changes

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -8,21 +8,20 @@
 --   let g:copilot_no_tab_map = v:true
 -- ]]
 
-local status_ok, copilot = pcall(require, "copilot")
-if not status_ok then
-  return
-end
-
-copilot.setup {
-  cmp = {
-    enabled = true,
-    method = "getPanelCompletions",
-  },
-  panel = { -- no config options yet
-    enabled = true,
-  },
-  ft_disable = { "markdown" },
-  -- plugin_manager_path = vim.fn.stdpath "data" .. "/site/pack/packer",
-  -- server_opts_overrides = {},
-}
-
+-- local status_ok, copilot = pcall(require, "copilot")
+-- if not status_ok then
+--   return
+-- end
+--
+-- copilot.setup {
+--   cmp = {
+--     enabled = true,
+--     method = "getPanelCompletions",
+--   },
+--   panel = { -- no config options yet
+--     enabled = true,
+--   },
+--   ft_disable = { "markdown" },
+--   -- plugin_manager_path = vim.fn.stdpath "data" .. "/site/pack/packer",
+--   -- server_opts_overrides = {},
+-- }
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -64,7 +64,11 @@ return packer.startup(function(use)
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "rktjmp/lush.nvim"
   use "lunarvim/darkplus.nvim"
+  use 'folke/tokyonight.nvim'
+  use "ellisonleao/gruvbox.nvim"
+  use "davidscotson/sonokai-nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
