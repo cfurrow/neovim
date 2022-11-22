4ea2a17029bae3f7edefb26f1bce5bbff27eb753
Carl Furrow <carl.furrow@betterup.co>
2022-11-22 09:44:11 -0500

    Add "live grep args" as default for <leader>F
    replaces the simpler "live grep"

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -93,7 +93,8 @@ local mappings = {
     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
     "Find files",
   },
-  ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
+  -- ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Live Grep" },
+  ["F"] = { "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>", "Live Grep with args" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
   p = {
@@ -160,6 +161,7 @@ local mappings = {
       "Workspace Symbols",
     },
   },
+
   s = {
     name = "Search",
     b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
