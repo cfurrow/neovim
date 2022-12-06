d96b3e11a2d1d3b67654730513643274be46cfef
Carl Furrow <carl.furrow@betterup.co>
2022-12-06 11:08:26 -0500

    Adjust indent

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -46,11 +46,11 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-      return
-    end
-    illuminate.on_attach(client)
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if not status_ok then
+    return
+  end
+  illuminate.on_attach(client)
   -- end
 end
 
