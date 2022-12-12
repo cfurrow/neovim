95c81cb260c7979c8f99283ae94d777359c4035c
Carl Furrow <hello@carlfurrow.com>
2022-12-12 18:54:51 -0500

    Set hidden

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -33,6 +33,7 @@ local options = {
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  hidden = true,                           -- With :set hidden, opening a new file when the current buffer has unsaved changes causes files to be hidden instead of closed
 }
 
 vim.opt.shortmess:append "c"
